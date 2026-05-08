1c23ba4828c63fcb77f7f2628f0def0e635ec7fa
jeffors <register.uzik@yandex.ru>
2026-05-08 23:45:14 +0500

    feat: update HomePage layout with hero background image and improved structure

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/ui/button'
 import config from '@/payload.config'
 import './styles.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import BackgroundImage from './../../../public/images/hero-bg.png'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -13,18 +15,12 @@ export default async function HomePage() {
   const { user } = await payload.auth({ headers })
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold font-heading">
-        {!user && 'Сайт нутрициолога.'}
-        {user && `Привет, ${user.email}`}
-      </h1>
-      <div className="flex min-h-svh items-center justify-center">
-        <Button className="font-bold w-full" size={'lg'}>
-          <Link href={payloadConfig.routes.admin} target="_blank">
-            Перейти в админ панель
-          </Link>
-        </Button>
-      </div>
+    <div className="overflow-x-hidden">
+      <section className="relative min-h-screen flex items-center pt-20">
+        <div className="absolute inset-0 z-0">
+          <Image src={BackgroundImage} alt="Hero" className="w-full h-full object-cover" />
+        </div>
+      </section>
     </div>
   )
 }
